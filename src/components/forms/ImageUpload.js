6265826cae1ed939bc1460aa80eaa7cb414c34ab
soft.web.development <softwebdevelopment@gmail.com>
2022-03-21 10:13:27 +0100

    first

--- src/components/forms/ImageUpload.js
+++ src/components/forms/ImageUpload.js
@@ -22,7 +22,7 @@ const ImageUpload = ({ values, setValues, setLoading, loading }) => {
       for (let i = 0; i < files.length; i++) {
         Resizer.imageFileResizer(
           files[i],
-          270,
+          260,
           500,
           "JPEG",
           100,
