9049e3d16d7220b4c9fb24842f53d89577f5cede
soft.web.development <softwebdevelopment@gmail.com>
2022-03-22 07:45:25 +0100

    first

--- src/components/forms/ContactSeller.js
+++ src/components/forms/ContactSeller.js
@@ -30,26 +30,46 @@ const ContactSeller = () => {
         toast.success('Message has been sent thanks for reaching out. You will be contacted shortly')
     }
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div className="contact-us-contents">
+      <div className="leave-a-comment">
+        <h3 className="theme-paragraph-title-h3">Contact Seller</h3>
+        <div className="single-blog-comment-form">
+          <form
+            action=""
+            method="POST"
+            className="contact-us-form"
+            onSubmit={sendEmail}
+            ref={form}
+          >
+            <input
+              type="text"
+              placeholder="Name (Required)"
+              id="id_name"
+              name="name"
+            />
+            <input
+              type="email"
+              placeholder="Email (Required)"
+              id="id_email"
+              name="email"
+            />
+            <input
+              type="text"
+              placeholder="Subject"
+              id="id_subject"
+              name="subject"
+            />
+            <textarea
+              cols="25"
+              rows="10"
+              id="id_message"
+              name="message"
+            ></textarea>
+            <button className="btn btn-success contact_submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
